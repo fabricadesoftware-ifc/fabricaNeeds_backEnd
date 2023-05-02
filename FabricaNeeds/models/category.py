c593198039d509a01cc5cd49f630297a075ebc81
HericMD <hericmdms@gmail.com>
2023-05-02 14:41:31 -0300

    Create Model Category #6

---
+++ FabricaNeeds/models/category.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Category(models.Model):
+    description = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.description
+    
+    class Meta:
+        verbose_name_plural = "Categories"
